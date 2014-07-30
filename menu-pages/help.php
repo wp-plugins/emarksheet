ae2ef74bf99db369cb20d8cda62273aaa5f9defb
rohitashv <rohitashv@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-30 13:45:51 +0000

    Frontend Result available now
    git-svn-id: https://plugins.svn.wordpress.org/emarksheet/tags/1.2@957392 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ menu-pages/help.php
@@ -0,0 +1,17 @@
+<link rel='stylesheet' type='text/css' href='<?php echo plugins_url('/bootstrap/css/bootstrap.css', __FILE__); ?>' />
+<link rel='stylesheet' type='text/css' href='<?php echo plugins_url('/bootstrap/css/bootstrap-responsive.css', __FILE__); ?>' />
+<div class=" span12 alert alert-info" style="margin-top:20px;">
+<h3>Help & Support</h3>
+<hr/>
+<strong>Creating Marksheet : </strong> <br/><br/>To Create a marksheet, just follow the following Steps : <br/><br/>
+Step 1 : Add Class <br/>
+Step 2 : Add subject to the class added in step 1 <br/>
+Step 3 : Enroll Student <br/>
+Step 4 : Add Marks<br/>
+Step 5 : Print Marksheet<br/><br/>
+<hr/>
+<strong>Launching Marksheet : </strong><br/><br/>
+To launch Result on your front end, just add shortcode <strong><em>[SHOW_EMARKSHEET]</em></strong> to your page or post and publish it
+<hr/>
+<strong>Regarding Support : </strong><br/><br/>
+in case of any support, you can send us mail at <strong><em>ucerturohit[at]gmail[dot]com</em></strong>
